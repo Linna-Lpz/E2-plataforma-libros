58c64132956b74ef70d9beef88389eede8001118
Eduardo <eduardo.perez.s@usach.cl>
2023-07-09 19:15:36 -0400

    factura

---
+++ BD/Factura.sql
@@ -0,0 +1,8 @@
+create table if not exists Factura
+(	id_factura int primary key,
+ 	Metodo_pago char NOT NULL,
+ 	correo char NOT NULL,
+ 	id_carrito int NOT NULL,
+ 	foreign key("Correo") references Usuario (Correo),
+ 	foreign key(id_carrito) references carrito (id_carrito)
+);
