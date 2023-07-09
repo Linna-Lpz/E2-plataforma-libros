dc078f290d1a6bcf527215eea108b54b080401a1
Eduardo <eduardo.perez.s@usach.cl>
2023-07-09 19:04:46 -0400

    me cambiaron el mer xd

--- BD/Carrito.sql
+++ BD/Carrito.sql
@@ -1,9 +1,6 @@
-create table public if not exists carrito
-(	"Id_libro" integer NOT NULL
- 	"Usuario_Id" int NOT NULL
- 	"Precio" int ()
- 	"cantidad" int(10) NOT NULL
- 	foreign key(Id_libro) references Libro(Id_libro)
- 	foreign key(Precio) references Libro(Id_libro)
- 	foreign key(Usuario_Id) references Usuarios(Usuario_Id)
+create table if not exists carrito
+(	id_carrito int primary key,
+ 	N_productos int NOT NULL,
+ 	ubicacion char NOT NULL,
+ 	saldo_total int NOT NULL,
 );
