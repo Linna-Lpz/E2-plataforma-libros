01c9b1945c76d5a843af25eb9fcd2cf3dcb2995c
prueba <catalina.lopez.a@usach.cl>
2023-07-11 15:15:12 -0400

    modificacion sintaxis ranking
    se añade una coma y comillas

--- BD/Ranking.sql
+++ BD/Ranking.sql
@@ -6,8 +6,8 @@ CREATE TABLE public."Ranking"
 (
     id_ranking integer NOT NULL,
     fk_Id_libro integer,
-    CONSTRAINT "Ranking_pkey" PRIMARY KEY (id_ranking)
-    CONSTRAINT fk_Id_libro FOREIGN KEY (fk_Id_libro) REFERENCES public."Libro"(Id_libro) MATCH SIMPLE
+    CONSTRAINT "Ranking_pkey" PRIMARY KEY (id_ranking),
+    CONSTRAINT fk_Id_libro FOREIGN KEY (fk_Id_libro) REFERENCES public."Libro"("Id_libro") MATCH SIMPLE
 
 )
 WITH (
