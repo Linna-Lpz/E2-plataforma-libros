d8e0771c9e3cb86f4f67ebe9f2785db365ce3d91
valecita <128949761+valecit-a@users.noreply.github.com>
2023-07-10 20:10:55 -0400

    Arreglando tablas
    Libro y valoracion

--- BD/LIBRO.sql
+++ BD/LIBRO.sql
@@ -5,17 +5,22 @@
 CREATE TABLE public."LIBRO"
 (
     "Año" date,
-    "Descripcion" "char" NOT NULL,
-    "Editorial" "char",
-    "Genero_libro" "char",
+    "Descripcion" text COLLATE pg_catalog."default" NOT NULL,
+    "Editorial" text COLLATE pg_catalog."default",
+    "Genero_libro" text COLLATE pg_catalog."default",
     "Id_libro" integer NOT NULL,
-    "Link" "char" NOT NULL,
-    "Nombre_libro" "char" NOT NULL,
+    "Link" text COLLATE pg_catalog."default" NOT NULL,
+    "Nombre_libro" text COLLATE pg_catalog."default" NOT NULL,
     "Precio" integer NOT NULL,
     "Stock" integer NOT NULL,
-    "Tipo_libro" "char" NOT NULL,
+    "Tipo_libro" text COLLATE pg_catalog."default" NOT NULL,
     "Numero_favoritos" integer NOT NULL,
+    fk_id_restriccion integer,
+    fk_id_ubicacion integer,
     CONSTRAINT "Libro_pkey" PRIMARY KEY ("Id_libro")
+    CONSTRAINT fk_id_ubicacion FOREIGN KEY (fk_id_ubicacion) REFERENCES public."Ubicacion"(id_ubicacion) MATCH SIMPLE
+    CONSTRAINT fk_id_restriccion FOREIGN KEY (fk_id_restriccion) REFERENCES public."Restriccion"(id_restriccion) MATCH SIMPLE
+
 )
 WITH (
     OIDS = FALSE
