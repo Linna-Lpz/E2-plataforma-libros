5e6bbc090dfd06310f34c49f056c990089042811
valecita <128949761+valecit-a@users.noreply.github.com>
2023-07-10 20:57:04 -0400

    Cambio en Libro

--- BD/LIBRO.sql
+++ BD/LIBRO.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE public."LIBRO"
 (
-    "Año" date,
+    "Año" integer,
     "Descripcion" text COLLATE pg_catalog."default" NOT NULL,
     "Editorial" text COLLATE pg_catalog."default",
     "Genero_libro" text COLLATE pg_catalog."default",
@@ -15,12 +15,9 @@ CREATE TABLE public."LIBRO"
     "Stock" integer NOT NULL,
     "Tipo_libro" text COLLATE pg_catalog."default" NOT NULL,
     "Numero_favoritos" integer NOT NULL,
-    fk_id_restriccion integer,
-    fk_id_ubicacion integer,
+    id_restriccion integer,
+    id_ubicacion integer,
     CONSTRAINT "Libro_pkey" PRIMARY KEY ("Id_libro")
-    CONSTRAINT fk_id_ubicacion FOREIGN KEY (fk_id_ubicacion) REFERENCES public."Ubicacion"(id_ubicacion) MATCH SIMPLE
-    CONSTRAINT fk_id_restriccion FOREIGN KEY (fk_id_restriccion) REFERENCES public."Restriccion"(id_restriccion) MATCH SIMPLE
-
 )
 WITH (
     OIDS = FALSE
