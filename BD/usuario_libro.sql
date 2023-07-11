ab70e28031745463b020720da32731ce5788c834
prueba <catalina.lopez.a@usach.cl>
2023-07-11 15:45:46 -0400

    coreccion de sintaxis
    tabla usuario libro corregida

--- BD/usuario_libro.sql
+++ BD/usuario_libro.sql
@@ -6,9 +6,11 @@ CREATE TABLE IF NOT EXISTS public."usuario_libro"
 (
     id_usuario_libro integer NOT NULL,
     favoritos char,
-    CONSTRAINT "usuario_libro_pkey" PRIMARY KEY (id_usuario_libro)
-    CONSTRAINT fk_usuario FOREIGN KEY (fk_usuario) REFERENCES public."Usuario"(correo) MATCH SIMPLE
-    CONSTRAINT fk_libro FOREIGN KEY (fk_libro) REFERENCES public."LIBRO"(Id_libro) MATCH SIMPLE
+    fk_usuario varchar,
+	fk_libro integer,
+    CONSTRAINT "usuario_libro_pkey" PRIMARY KEY (id_usuario_libro),
+    CONSTRAINT fk_usuario FOREIGN KEY (fk_usuario) REFERENCES public."Usuario"(correo) MATCH SIMPLE,
+    CONSTRAINT fk_libro FOREIGN KEY (fk_libro) REFERENCES public."Libro"("Id_libro") MATCH SIMPLE
 )
 
 TABLESPACE pg_default;
