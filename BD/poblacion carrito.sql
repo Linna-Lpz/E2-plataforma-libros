6da6d2224215f10e3a5f578ee83e78f0e9c79ef1
Eduardo <eduardo.perez.s@usach.cl>
2023-07-12 18:45:15 -0400

    sentencia precio total carrito y población carrito. Utilizar este archivo para las demas poblaciones

---
+++ BD/poblacion carrito.sql
@@ -0,0 +1,8 @@
+INSERT INTO carrito (id_carrito, N_productos, ubicacion, saldo_total, fk_correo_carrito)
+VALUES
+  (1, 3, 'Ubicacion A', 150, 12345),
+  (2, 2, 'Ubicacion B', 75, 54321),
+  (3, 5, 'Ubicacion C', 200, 98765),
+  (4, 4, 'Ubicacion D', 120, 45678),
+  (5, 1, 'Ubicacion A', 50, 23456),
+  (6, 6, 'Ubicacion B', 180, 87654);
